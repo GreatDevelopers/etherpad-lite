9b34e93dd25bc0e5cf45232f5897d97ca0f4bf7a
Kenton Varda <kenton@sandstorm.io>
2017-03-07 13:59:19 -0800

    Fix minification script to account for embedded CSS in JS.
    Apparently, pad.js contains CSS files embedded as strings.
    
    Etherpad normally performs minification at runtime, and does some crazy loopback stuff to its own minification engine to cause these embeds themselves to be minified.
    
    Sandstorm prefers minification in advance, via this script. But the script didn't account for embedded strings, so they didn't get minified. This not only meant that pad.js was larger than it was supposed to be, but it broke @import statements inside the CSS blobs.
    
    This used not to matter because there were no @imports. But in 1.6.1, CSS for bullet/numbered lists was moved into a separate file and @imported. So, it broke.
    
    Fixes https://github.com/sandstorm-io/sandstorm/issues/2881

--- minify-cache.sh
+++ minify-cache.sh
@@ -1,11 +1,35 @@
 #! /bin/bash
 
+set -euo pipefail
+
+minify_embeds() {
+  # TODO(someday): Minify JS too. But require-kernel.js seems to be concocted from thin air.
+  local pattern='^Ace2Editor\.EMBEDED\["\.\./([^"]*\.css)"] = '
+  while read -r LINE; do
+    if [[ "$LINE" =~ $pattern ]]; then
+      embed=${BASH_REMATCH[1]}
+      echo -n "Ace2Editor.EMBEDED[\"../$embed\"] = \""
+      cleancss src/$embed | sed -e 's/"/\\"/g'
+      echo "\";"
+    else
+      echo "$LINE"
+    fi
+  done
+}
+
 for file in cache/*.js%3F*; do
   if [ ${file%.gz} != $file ]; then continue; fi
   if [ ${file%.ugly} != $file ]; then continue; fi
   if grep -q already_ugly $file; then continue; fi
+
+  echo "**** $file" >&2
   
-  echo $file
+  if egrep -q '^Ace2Editor\.EMBEDED\["\.\./([^"]*\.css)"] = ' $file; then
+    echo "* minifying embeds too" >&2
+    minify_embeds < $file > $file.reembed
+    mv $file.reembed $file
+  fi
+
   uglify -s $file -o $file.ugly
   echo "//already_ugly" >> $file.ugly
   mv $file.ugly $file
@@ -17,7 +41,13 @@ for file in cache/*.css; do
   if [ ${file%.gz} != $file ]; then continue; fi
   if [ ${file%.ugly} != $file ]; then continue; fi
   if grep -q already_ugly $file; then continue; fi
-  
+
+  echo "**** $file" >&2
+
+  if grep -q "@import url([\"']./" $file; then
+    sed -i -re "s,@import url\(([\"'])\./,@import url(\1../src/static/css/,g" $file
+  fi
+
   echo $file
   cleancss -o $file.ugly $file
   echo "/*already_ugly*/" >> $file.ugly
